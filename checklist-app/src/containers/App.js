1ea6c11b96c2edb94dd20f5f575155bf1a84b88f
FrancisBroughton <iamfrancisbroughton@gmail.com>
2018-01-10 16:40:53 +0000

    static addItem component rendering

--- checklist-app/src/containers/App.js
+++ checklist-app/src/containers/App.js
@@ -6,6 +6,20 @@ class App extends Component {
     return (
       <div>
         Hello world! The Page is rendering!
+      
+        <div class="container-fluid">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" 
+              placeholder="Add Item...." 
+              aria-label="Add Item...." 
+              aria-describedby="basic-addon2"/>
+            <div class="input-group-append">
+              <button type="button" class="btn btn-outline-secondary">Add item</button>
+            </div>
+           
+          </div>
+        
+        </div>
       </div>
     );
   }
